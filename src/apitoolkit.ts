e7e6ce418c50f13bce97197fbd2bc77c06b39561
dawkaka <yousiph77@gmail.com>
2024-10-28 12:56:00 +0000

    cleanup codebase

--- src/apitoolkit.ts
+++ src/apitoolkit.ts
@@ -1,11 +1,10 @@
-import fetch from "sync-fetch";
-import { v4 as uuidv4 } from "uuid";
-
-import { trace } from "@opentelemetry/api";
-import { Application, NextFunction, Request, Response } from "express";
-import { redactFields } from "apitoolkit-js/lib/payload";
-import { asyncLocalStorage, ReportError } from "apitoolkit-js";
-export { ReportError } from "apitoolkit-js";
+import fetch from 'sync-fetch';
+import { v4 as uuidv4 } from 'uuid';
+import { Span, trace } from '@opentelemetry/api';
+import { Application, NextFunction, Request, Response } from 'express';
+import { redactFields } from 'apitoolkit-js/lib/payload';
+import { asyncLocalStorage, ReportError } from 'apitoolkit-js';
+export { ReportError } from 'apitoolkit-js';
 
 export type Config = {
   apiKey: string;
@@ -23,8 +22,6 @@ export type Config = {
 
 type ClientMetadata = {
   project_id: string;
-  pubsub_project_id: string;
-  topic_id: string;
 };
 
 export class APIToolkit {
@@ -49,52 +46,37 @@ export class APIToolkit {
     this.expressMiddleware = this.expressMiddleware.bind(this);
   }
 
-  public expressErrorHandler(
-    err: Error,
-    _req: Request,
-    _res: Response,
-    next: NextFunction
-  ) {
+  public expressErrorHandler(err: Error, _req: Request, _res: Response, next: NextFunction) {
     ReportError(err);
     next(err);
   }
-  public errorHandler(
-    err: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
+  public errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
     return this.expressErrorHandler(err, req, res, next);
   }
 
   public expressMiddleware(req: Request, res: Response, next: NextFunction) {
     if (this.project_id === undefined) {
-      console.log(
-        "APIToolkit: expressMiddleware called, but apitoolkit was not correctly setup. Doing nothing."
-      );
+      console.log('APIToolkit: expressMiddleware called, but apitoolkit was not correctly setup. Doing nothing.');
       next();
       return;
     }
-
     asyncLocalStorage.run(new Map(), () => {
       const store = asyncLocalStorage.getStore();
+      const msg_id = uuidv4();
+      const span = trace.getTracer(this.serviceName).startSpan('apitoolkit-http-span');
 
-      const msg_id: string = uuidv4();
-      const tracer = trace.getTracer(this.serviceName);
-      const span = tracer.startSpan("apitoolkit-http-span");
       if (store) {
-        store.set("apitoolkit-span", span);
-        store.set("apitoolkit-msg-id", msg_id);
-        store.set("AT_errors", []);
+        store.set('apitoolkit-span', span);
+        store.set('apitoolkit-msg-id', msg_id);
+        store.set('AT_errors', []);
       }
-
       if (this.config?.debug) {
-        console.log("APIToolkit: expressMiddleware called");
+        console.log('APIToolkit: expressMiddleware called');
       }
 
-      let respBody: any = "";
+      let respBody: any = '';
       const oldSend = res.send;
-      res.send = (val) => {
+      res.send = val => {
         if (this.captureResponseBody) {
           respBody = val;
         }
@@ -102,108 +84,13 @@ export class APIToolkit {
       };
 
       const onRespFinished = (req: Request, res: Response) => () => {
-        res.removeListener("close", onRespFinished(req, res));
-        res.removeListener("error", onRespFinished(req, res));
-        res.removeListener("finish", onRespFinished(req, res));
+        res.removeListener('close', onRespFinished(req, res));
+        res.removeListener('error', onRespFinished(req, res));
+        res.removeListener('finish', onRespFinished(req, res));
         try {
-          let reqBody = "";
-          if (req.body && this.captureRequestBody) {
-            try {
-              if (req.is("multipart/form-data")) {
-                if (req.file) {
-                  req.body[req.file.fieldname] = `[${req.file.mimetype}_FILE]`;
-                } else if (req.files) {
-                  if (!Array.isArray(req.files)) {
-                    for (const file in req.files) {
-                      req.body[file] = (req.files[file] as any).map(
-                        (f: any) => `[${f.mimetype}_FILE]`
-                      );
-                    }
-                  } else {
-                    for (const file of req.files) {
-                      req.body[file.fieldname] = `[${file.mimetype}_FILE]`;
-                    }
-                  }
-                }
-              }
-              reqBody = JSON.stringify(req.body);
-            } catch {
-              reqBody = String(req.body);
-            }
-          }
-          let url_path = req.route?.path || "";
-          if (url_path == "" && req.method.toLowerCase() !== "head") {
-            url_path = findMatchedRoute(req.app, req.method, req.originalUrl);
-          } else if (req.baseUrl && req.baseUrl != "") {
-            if (req.originalUrl.startsWith(req.baseUrl)) {
-              url_path = req.baseUrl + url_path;
-            } else {
-              url_path = findMatchedRoute(req.app, req.method, req.originalUrl);
-            }
-          }
-          if (span) {
-            span.setAttribute("net.host.name", req.hostname);
-            span.setAttribute("at-project-key", this.apitoolkit_key || "");
-            span.setAttribute("apitoolkit.msg_id", msg_id);
-            span.setAttribute("http.route", url_path);
-            span.setAttribute("http.request.method", req.method);
-            span.setAttribute("http.response.status_code", res.statusCode);
-            span.setAttribute(
-              "http.request.query_params",
-              JSON.stringify(req.query)
-            );
-            span.setAttribute(
-              "http.request.path_params",
-              JSON.stringify(req.params)
-            );
-            span.setAttribute("apitoolkit.sdk_type", "JsExpress");
-            const reqHeaders = Object.entries(req.headers);
-
-            reqHeaders.forEach(([header, value]) => {
-              const isRedacted = this.config.redactHeaders?.some(
-                (h) =>
-                  h.toLowerCase() === header.toLowerCase() ||
-                  ["cookie", "authorization"].includes(h.toLowerCase())
-              );
-              const headerVal = isRedacted
-                ? "[CLIENT_REDACTED]"
-                : String(value);
-              span.setAttribute("http.request.header." + header, headerVal);
-            });
-            const resHeaders = Object.entries(res.getHeaders());
-            resHeaders.forEach(([header, value]) => {
-              const isRedacted = this.config.redactHeaders?.some(
-                (h) =>
-                  h.toLowerCase() === header.toLowerCase() ||
-                  ["cookie", "authorization"].includes(h.toLowerCase())
-              );
-              const headerVal = isRedacted
-                ? "[CLIENT_REDACTED]"
-                : String(value);
-              span.setAttribute("http.response.header." + header, headerVal);
-            });
-            span.setAttribute(
-              "http.request.body",
-              Buffer.from(
-                redactFields(reqBody, this.config.redactRequestBody || [])
-              ).toString("base64")
-            );
-            span.setAttribute(
-              "http.response.body",
-              Buffer.from(
-                redactFields(respBody, this.config.redactRequestBody || [])
-              ).toString("base64")
-            );
-            span.setAttribute(
-              "apitoolkit.errors",
-              JSON.stringify(store?.get("AT_errors") || [])
-            );
-            span.setAttribute(
-              "apitoolkit.service_version",
-              this.config.serviceVersion || ""
-            );
-            span.setAttribute("apitoolkit.tags", this.config.tags || []);
-          }
+          const reqBody = this.getRequestBody(req);
+          const url_path = this.getUrlPath(req);
+          this.setAttributes(span, req, res, msg_id, url_path, reqBody, respBody);
         } catch (error) {
           if (this.config?.debug) {
             console.log(error);
@@ -214,84 +101,139 @@ export class APIToolkit {
       };
 
       const onRespFinishedCB = onRespFinished(req, res);
-      res.on("finish", onRespFinishedCB);
-      res.on("error", onRespFinishedCB);
-      try {
-        next();
-      } catch (error) {
-        next(error);
-      }
+      res.on('finish', onRespFinishedCB).on('error', onRespFinishedCB);
+      next();
     });
   }
   public ReportError = ReportError;
 
   static NewClient(config: Config) {
-    const { rootURL = "https://app.apitoolkit.io" } = config;
+    const { rootURL = 'https://app.apitoolkit.io' } = config;
 
-    if (config.apiKey != "") {
+    if (config.apiKey != '') {
       const clientMeta = this.getClientMetadata(rootURL, config.apiKey);
       return new APIToolkit(config, config.apiKey, clientMeta?.project_id);
     }
-    if (config.apiKey == "") {
-      console.error("APIToolkit: apiKey is required");
+    if (config.apiKey == '') {
+      console.error('APIToolkit: apiKey is required');
     }
     return new APIToolkit(config, config.apiKey, undefined);
   }
 
   static getClientMetadata(rootURL: string, apiKey: string) {
-    const resp = fetch(rootURL + "/api/client_metadata", {
-      method: "GET",
+    const resp = fetch(rootURL + '/api/client_metadata', {
+      method: 'GET',
       headers: {
-        Authorization: "Bearer " + apiKey,
-        Accept: "application/json",
-      },
+        Authorization: 'Bearer ' + apiKey,
+        Accept: 'application/json'
+      }
     });
     if (!resp.ok) {
       if (resp.status === 401) {
-        throw new Error("APIToolkit: Invalid API Key");
+        throw new Error('APIToolkit: Invalid API Key');
       }
       console.error(`Error getting apitoolkit client_metadata ${resp.status}`);
       return;
     }
     return resp.json() as ClientMetadata;
   }
+
+  private setAttributes(span: Span, req: Request, res: Response, msg_id: string, urlPath: string, reqBody: string, respBody: string) {
+    span.setAttributes({
+      'net.host.name': req.hostname,
+      'at-project-key': this.apitoolkit_key || '',
+      'apitoolkit.msg_id': msg_id,
+      'http.route': urlPath,
+      'http.request.method': req.method,
+      'http.response.status_code': res.statusCode,
+      'http.request.query_params': JSON.stringify(req.query),
+      'http.request.path_params': JSON.stringify(req.params),
+      'apitoolkit.sdk_type': 'JsExpress',
+      'http.request.body': Buffer.from(redactFields(reqBody, this.config.redactRequestBody || [])).toString('base64'),
+      'http.response.body': Buffer.from(redactFields(respBody, this.config.redactRequestBody || [])).toString('base64'),
+      'apitoolkit.errors': JSON.stringify(asyncLocalStorage.getStore()?.get('AT_errors') || []),
+      'apitoolkit.service_version': this.config.serviceVersion || '',
+      'apitoolkit.tags': this.config.tags || []
+    });
+
+    const redactHeader = (header: string) =>
+      this.config.redactHeaders?.includes(header.toLowerCase()) || ['cookies', 'authorization'].includes(header.toLowerCase())
+        ? '[CLIENT_REDACTED]'
+        : header;
+
+    Object.entries(req.headers).forEach(([header, value]) => span.setAttribute(`http.request.header.${header}`, redactHeader(String(value))));
+    Object.entries(res.getHeaders()).forEach(([header, value]) => span.setAttribute(`http.response.header.${header}`, redactHeader(String(value))));
+  }
+
+  private getRequestBody(req: Request): string {
+    const reqBody = '';
+    if (req.body && this.captureRequestBody) {
+      try {
+        if (req.is('multipart/form-data')) {
+          if (req.file) {
+            req.body[req.file.fieldname] = `[${req.file.mimetype}_FILE]`;
+          } else if (req.files) {
+            if (!Array.isArray(req.files)) {
+              for (const file in req.files) {
+                req.body[file] = (req.files[file] as any).map((f: any) => `[${f.mimetype}_FILE]`);
+              }
+            } else {
+              for (const file of req.files) {
+                req.body[file.fieldname] = `[${file.mimetype}_FILE]`;
+              }
+            }
+          }
+        }
+        return JSON.stringify(req.body);
+      } catch {
+        return String(req.body);
+      }
+    }
+    return reqBody;
+  }
+
+  private getUrlPath(req: Request): string {
+    let url_path = req.route?.path || '';
+    if (url_path == '' && req.method.toLowerCase() !== 'head') {
+      url_path = findMatchedRoute(req.app, req.method, req.originalUrl);
+    } else if (req.baseUrl && req.baseUrl != '') {
+      if (req.originalUrl.startsWith(req.baseUrl)) {
+        url_path = req.baseUrl + url_path;
+      } else {
+        url_path = findMatchedRoute(req.app, req.method, req.originalUrl);
+      }
+    }
+    return url_path;
+  }
 }
 
-export const findMatchedRoute = (
-  app: Application,
-  method: string,
-  url: string
-): string => {
+export const findMatchedRoute = (app: Application, method: string, url: string): string => {
   try {
-    const path = url.split("?")[0];
+    const path = url.split('?')[0];
     const stack = app._router.stack;
-    let final_path = "";
+    let final_path = '';
 
     const gatherRoutes = (stack: any, build_path: string, path: string) => {
       for (const layer of stack) {
         if (layer.route) {
-          if (
-            path.startsWith(layer.path) &&
-            layer.route.methods[method.toLowerCase()] &&
-            (layer.path === path || layer.regex.test(path))
-          ) {
+          if (path.startsWith(layer.path) && layer.route.methods[method.toLowerCase()] && (layer.path === path || layer.regex.test(path))) {
             build_path += layer.route.path;
             final_path = build_path;
             return;
           }
-        } else if (layer.name === "router" && layer.handle.stack) {
+        } else if (layer.name === 'router' && layer.handle.stack) {
           if (path.startsWith(layer.path)) {
             build_path += transformPath(layer.params, layer.path);
-            path = path.replace(layer.path, "");
+            path = path.replace(layer.path, '');
             gatherRoutes(layer.handle.stack, build_path, path);
           }
         }
       }
     };
-    gatherRoutes(stack, "", path);
+    gatherRoutes(stack, '', path);
     return final_path;
   } catch {
-    return "";
+    return '';
   }
 };
 
